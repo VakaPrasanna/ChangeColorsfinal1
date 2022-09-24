b39b827e1d0f7d874cc413a9670964a94b81005f
VakaPrasanna <96370281+VakaPrasanna@users.noreply.github.com>
2022-09-24 10:26:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import ChangeColors from "./ChangeColors.js";
+export default function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <ChangeColors />
+    </div>
+  );
+}
